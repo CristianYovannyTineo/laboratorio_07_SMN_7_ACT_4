13352a381a26c42d3429f5606a2a4fde60dd53f8
USUARIO <USUARIO@LAPTOP-BUKF8MRB>
2023-10-04 23:31:54 -0500

    Agregar archivos de proyecto.

---
+++ laboratorio_07_SMN_7_ACT_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Write(" Ingrese un número: ");
+string Pr = Console.ReadLine();
+if (int.TryParse(Pr, out int numero) && numero > 0)
+{
+     for (int i = 0; i < numero; i++)
+     {
+        for (int j = 0; j < numero * 2; j++)
+        {
+        Console.Write("*");
+        }
+        Console.WriteLine();
+     }
+}
